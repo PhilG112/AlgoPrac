88893fc13d50e2128b9cbe6ab3622a14d18e8454
phillip-grujovski <phillip@jayride.com>
2019-10-23 09:36:49 +1100

    .

--- AlgoPrac.App/Algorithms/InsertionSort/InsertionSort.cs
+++ AlgoPrac.App/Algorithms/InsertionSort/InsertionSort.cs
@@ -4,23 +4,37 @@ namespace AlgoPrac.App.Algorithms.InsertionSort
 {
     public static class InsertionSort
     {
-        public static IList<int> InsertionSortAlgorithm(IList<int> a)
+        public static IList<int> InsertionSortAlgorithm(IList<int> array)
         {
-            for (var j = 2; j < a.Count; j++)
+            for (var i = 1; i < array.Count; i++)
             {
-                var key = a[j];
-                var i = j -1;
+                // Get the current element from the array to compare
+                var key = array[i];
 
-                while(i > 0 && a[i] > key)
+                // Set j to one behind index i to compare
+                var j = i - 1;
+
+                while(
+                    // Check if j is greater than or equal to 0
+                    j >= 0 &&
+                    // Check if element at index j is greater than the key at index i
+                    array[j] > key)
                 {
-                    a[i + 1] = a[i];
-                    i = i - 1;
+                    // Set array element at index j + 1 to be the higher element
+                    // E.g if array[j + 1] is 3 and array[j] is 8, we set array[j + 1] to be 8
+                    // Which will look like this [ 8, 8 ]
+                    array[j + 1] = array[j];
+
+                    // Set j to be the previous element index
+                    j = j - 1;
                 }
 
-                a[i + 1] = key;
+                // Increment index j by one and set it to the key which is the lower element
+                // Which will look like this [ 3, 8 ]
+                array[j + 1] = key;
             }
 
-            return a;
+            return array;
         }
     }
 }
